a9050d6fa1b81acf1f6cb9d4b8004733de82a7a5
zsauce001 <99756685+zsauce001@users.noreply.github.com>
2026-04-07 14:07:48 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
+const CACHE_NAME = "offline-qr-form-v1";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./html5-qrcode.min.js",
+  "./icon_red.png",
+  "./manifest.webmanifest"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    }).catch(() => caches.match("./index.html"))
+  );
+});
